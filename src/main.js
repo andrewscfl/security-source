c88528114333a5525368623b06989cb8226cd4db
andrew <andrewscaldwell@gmail.com>
2022-10-04 15:01:25 -0400

    added state management and router

--- src/main.js
+++ src/main.js
@@ -1,10 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import { createPinia, PiniaVuePlugin } from 'pinia'
+import VueRouter from 'vue-router';
+import HomeView from './views/HomeView.vue'
+
+Vue.use(VueRouter);
+Vue.use(PiniaVuePlugin)
+
+const pinia = createPinia()
+const router = new VueRouter({
+  routes: [
+    { path: '/', component: HomeView },
+  ]
+});
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
+  pinia,
+  router
 }).$mount('#app')
